d4179e3f04a2822b36d097ad27da8fdbc91d92ca
Fedor4994 <fedorsosnin04@gmail.com>
2022-12-29 20:57:39 +0200

    Add profile page

---
+++ src/components/Tooling/Tooling..jsx
@@ -0,0 +1,58 @@
+import s from './Tooling.module.css';
+import {
+  SiJavascript,
+  SiRedux,
+  SiHtml5,
+  SiCss3,
+  SiGithub,
+  SiFigma,
+} from 'react-icons/si';
+import { RiReactjsLine } from 'react-icons/ri';
+import { FaSass, FaGitAlt } from 'react-icons/fa';
+const Tooling = () => {
+  return (
+    <div className={s.tooling}>
+      <h3 className={s.skills}>SKILLS:</h3>
+      <ul className={s.toolsList}>
+        <li className={s.tool}>
+          <SiJavascript color="#00c6ed" size={40} />
+          JavaScript
+        </li>
+        <li className={s.tool}>
+          <RiReactjsLine color="#00c6ed" size={45} />
+          React
+        </li>
+        <li className={s.tool}>
+          <SiRedux color="#00c6ed" size={40} />
+          Redux
+        </li>
+        <li className={s.tool}>
+          <SiHtml5 color="#00c6ed" size={40} />
+          HTML5
+        </li>
+        <li className={s.tool}>
+          <SiCss3 color="#00c6ed" size={40} />
+          CSS3
+        </li>
+        <li className={s.tool}>
+          <FaSass color="#00c6ed" size={45} />
+          SCSS
+        </li>
+        <li className={s.tool}>
+          <FaGitAlt color="#00c6ed" size={45} />
+          Git
+        </li>
+        <li className={s.tool}>
+          <SiGithub color="#00c6ed" size={40} />
+          GitHub
+        </li>
+        <li className={s.tool}>
+          <SiFigma color="#00c6ed" size={40} />
+          Figma
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Tooling;
--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -1,8 +1,35 @@
+import Tooling from 'components/Tooling/Tooling.';
 import Layout from 'views/Layout/Layout';
+import profilePhoto from './avatar.jpeg';
+import s from './Profile.module.css';
+
 const Profile = () => {
   return (
     <Layout>
-      <h1>Profile</h1>
+      <section className={s.profile}>
+        <p className={s.title}>Profile</p>
+
+        <img className={s.profilePhoto} src={profilePhoto} alt="profile" />
+        <div className={s.about}>
+          <h2 className={s.me}>
+            I'm a front-end developer located in Ukraine.
+          </h2>
+
+          <p>
+            At the moment of my life, programming is the main occupation, where
+            I use all my time to get new information. Ready to learn the
+            technologies you need, and develop with you. At this stage, I am
+            looking for a company not so much for the financial component, but
+            for professional growth as a developer. The quickest way to contact
+            me would be through&nbsp;
+            <a href="https://t.me/TrySigma" target="_blank" rel="noreferrer">
+              Telegram.&nbsp;
+            </a>
+            I can also be reached by other resume contact information.
+          </p>
+        </div>
+        <Tooling />
+      </section>
     </Layout>
   );
 };
