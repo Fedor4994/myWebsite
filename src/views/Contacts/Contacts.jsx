7f7de518e458e6be08bd780b54b3ca3e45e94330
Fedor4994 <103943126+Fedor4994@users.noreply.github.com>
2023-01-02 00:43:09 +0200

    Add contacts page

--- src/views/Contacts/Contacts.jsx
+++ src/views/Contacts/Contacts.jsx
@@ -1,7 +1,52 @@
 import Layout from 'views/Layout/Layout';
+import s from './Contacts.module.css';
 
 const Contacts = () => {
-  return <Layout>Contacts</Layout>;
+  return (
+    <Layout>
+      <section className={s.contacts}>
+        <p className={s.title}>Contact Me</p>
+        <p className={s.about}>
+          The quickest way to contact me would be through&nbsp;
+          <a
+            className={s.link}
+            href="https://t.me/TrySigma"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Telegram.&nbsp;
+          </a>
+          <br /> <br /> I can also be reached by email:&nbsp;
+          <a
+            className={s.link}
+            href="mailto:fedorsosnin04@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            fedorsosnin04@gmail.com
+          </a>
+          <a
+            className={s.link}
+            href="https://www.linkedin.com/in/fedor-sosnin-4686b425a/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            , Linkedin
+          </a>
+          , and other&nbsp;
+          <a
+            className={s.link}
+            href="https://translate.google.ru/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            resume&nbsp;
+          </a>
+          contact information.
+        </p>
+      </section>
+    </Layout>
+  );
 };
 
 export default Contacts;
